a1738a4dd71e4284d81bbbd6f575a665b2b9c874
justgrace <ivan.koliada69@gmail.com>
2020-09-29 18:50:21 +0300

    feat: add solutions extended repeater task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,10 +4,7 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-   return false;
+   return !(sampleActivity) || isNaN(sampleActivity) || typeof sampleActivity !== 'string' || +sampleActivity > 15 || +sampleActivity <= 0 ? false : Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
 
 
-
-
-// };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,9 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-   throw new CustomError('Not implemented');
-   // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+   options['separator'] = options['separator'] || '+';
+   options['additionSeparator'] = options['additionSeparator'] || '|';
+   if (typeof options['addition'] !== 'string' && typeof str !== 'string') {
+      options['addition'] += '';
+      str += '';
+   }
+
+   let result = '',
+      addit = '';
+
+   if (options['addition']) {
+      addit = options['addition'];
+   } else addit += '';
+
+   if (options['additionRepeatTimes']) {
+      addit = (addit + options['additionSeparator']).repeat(options['additionRepeatTimes'] - 1) + addit;
+   } else addit += '';
+
+   if (options['repeatTimes']) {
+      result = (str + addit + options['separator']).repeat(options['repeatTimes'] - 1) + str + addit;
+   } else result = str + addit;
+
+   return result;
 };
-
-
-
